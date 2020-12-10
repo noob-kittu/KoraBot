a9d596b1301eb9381b9ad0b024806e010afbac95
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-10 19:03:37 +0530

    Update music.py

--- tg_bot/modules/music.py
+++ tg_bot/modules/music.py
@@ -13,6 +13,7 @@ from base64 import b64decode
 from pySmartDL import SmartDL
 from telethon.tl.types import DocumentAttributeVideo, DocumentAttributeAudio
 from telethon import events
+
 from tg_bot.events import register
 from tg_bot.modules.helper_funcs.utils import progress
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -35,9 +36,9 @@ try:
    from youtubesearchpython import SearchVideos 
 
 except:
-    os.system("pip install pip install youtube-search-python")
-    from youtubesearchpython import SearchVideos 
-    pass
+	os.system("pip install pip install youtube-search-python")
+	from youtubesearchpython import SearchVideos 
+	pass
 
 @register(pattern="^/song (.*)")
 async def download_video(v_url):
@@ -47,7 +48,7 @@ async def download_video(v_url):
     if not sender.id == me.id:
         rkp = await lazy.reply("`processing...`")
     else:
-        rkp = await lazy.edit("`processing...`")   
+    	rkp = await lazy.edit("`processing...`")   
     url = v_url.pattern_match.group(1)
     if not url:
          return await rkp.edit("`Error \nusage song <song name>`")
@@ -58,7 +59,7 @@ async def download_video(v_url):
     try:
        url = q[0]['link']
     except:
-        return await rkp.edit("`failed to find`")
+    	return await rkp.edit("`failed to find`")
     type = "audio"
     await rkp.edit("`Preparing to download...`")
     if type == "audio":
@@ -157,8 +158,129 @@ async def download_video(v_url):
                 progress(d, t, v_url, c_time, "Uploading..",
                          f"{rip_data['title']}.mp4")))
         os.remove(f"{rip_data['id']}.mp4")
+
+
+@register(pattern="^/video (.*)")
+async def download_video(v_url):  
+    lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
+    if not sender.id == me.id:
+        rkp = await lazy.reply("`processing...`")
+    else:
+    	rkp = await lazy.edit("`processing...`")   
+    url = v_url.pattern_match.group(1)
+    if not url:
+         return await rkp.edit("`Error \nusage song <song name>`")
+    search = SearchVideos(url, offset = 1, mode = "json", max_results = 1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get('search_result')
+    try:
+       url = q[0]['link']
+    except:
+    	return await rkp.edit("`failed to find`")
+    type = "audio"
+    await rkp.edit("`Preparing to download...`")
+    if type == "audio":
+        opts = {
+            'format':
+            'best',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4'
+            }],
+            'outtmpl':
+            '%(id)s.mp4',
+            'logtostderr':
+            False,
+            'quiet':
+            True
+        }
+        song = False
+        video = True
+    try:
+        await rkp.edit("`Fetching data, please wait..`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await rkp.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await rkp.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await rkp.edit(
+            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
+        )
+        return
+    except MaxDownloadsReached:
+        await rkp.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await rkp.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await rkp.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        await rkp.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        return
+    except ExtractorError:
+        await rkp.edit("`There was an error during info extraction.`")
+        return
+    except Exception as e:
+        await rkp.edit(f"{str(type(e)): {str(e)}}")
+        return
+    c_time = time.time()
+    if song:
+        await rkp.edit(f"`Preparing to upload song `\
+        \n**{rip_data['title']}**\
+        \nby *{rip_data['uploader']}*")
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp3",
+            supports_streaming=True,
+            attributes=[
+                DocumentAttributeAudio(duration=int(rip_data['duration']),
+                                       title=str(rip_data['title']),
+                                       performer=str(rip_data['uploader']))
+            ],
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{rip_data['title']}.mp3")))
+        os.remove(f"{rip_data['id']}.mp3")
+        await v_url.delete()
+    elif video:
+        await rkp.edit(f"`Preparing to upload video song :`\
+        \n**{rip_data['title']}**\
+        \nby *{rip_data['uploader']}*")
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp4",
+            supports_streaming=True,
+            caption=rip_data['title'],
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{rip_data['title']}.mp4")))
+        os.remove(f"{rip_data['id']}.mp4")
+        await rkp.delete()
+
+
 __help__ = """
-  - /song <name of song > :- get song in High quality uploaded to u
-  - /video <link/songname >: download video songs from youtube
+  /song <songname artist(optional)>: uploads the song in it's best quality available
+
+  /video <songname artist(optional)>: uploads the video song in it's best quality available
 """
-__mod_name__ = "Song"
+
+__mod_name__ = "SONGS"
