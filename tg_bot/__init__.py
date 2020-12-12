ec2f43b9ccaa52507c9a2d03fad703f4c840d34e
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2020-12-12 18:49:07 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -61,6 +61,7 @@ if ENV:
         raise Exception("Your developer users list does not contain valid integers.")
 
     GBAN_LOGS = os.environ.get('GBAN_LOGS', None)
+    REDIS_URL =os.environ.get('REDIS_URL')
     WEBHOOK = bool(os.environ.get('WEBHOOK', False))
     URL = os.environ.get('URL', "")  # Does not contain token
     PORT = int(os.environ.get('PORT', 5000))
@@ -134,7 +135,6 @@ else:
     GBAN_LOGS = Config.GBAN_LOGS
     WEBHOOK = Config.WEBHOOK
     URL = Config.URL
-    REDIS_URL =os.environ.get('REDIS_URL')
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
     API_ID = Config.API_ID
