209bddcf03c94e28d7ff87c3e100e852b8e79ac6
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2020-12-12 19:45:27 +0530

    Update alternate.py

--- tg_bot/modules/helper_funcs/alternate.py
+++ tg_bot/modules/helper_funcs/alternate.py
@@ -1,12 +1,13 @@
+from telegram.error import BadRequest
 from functools import wraps
-from telegram import error, ChatAction
+from telegram import ChatAction
 from telegram import User, Chat, ChatMember, Update, Bot
-from telegram import error
+
 
 def send_message(message, text, *args, **kwargs):
     try:
         return message.reply_text(text, *args, **kwargs)
-    except error.BadRequest as err:
+    except BadRequest as err:
         if str(err) == "Reply message not found":
             return message.reply_text(text, quote=False, *args, **kwargs)
 
@@ -15,26 +16,9 @@ def typing_action(func):
     """Sends typing action while processing func command."""
 
     @wraps(func)
-    def command_func(bot: Bot, update: Update, *args, **kwargs):
-        bot.send_chat_action(
-            chat_id=update.effective_chat.id, action=ChatAction.TYPING
-        )
-        return func(bot: Bot, update: Update, *args, **kwargs)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+        return func(update, context, *args, **kwargs)
 
     return command_func
-
-
-def send_action(action):
-    """Sends `action` while processing func command."""
-
-    def decorator(func):
-        @wraps(func)
-        def command_func(bot: Bot, update: Update, *args, **kwargs):
-            bot.send_chat_action(
-                chat_id=update.effective_chat.id, action=action
-            )
-            return func(bot: Bot, update: Update, *args, **kwargs)
-
-        return command_func
-
-    return decorator
