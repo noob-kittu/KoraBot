d272bbb7d622e9878c3b72dfe29ff942a749172d
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2020-12-16 15:54:58 +0530

    Revert "Update users_sql.py"
    This reverts commit 5e3d2e3256460dddc6b4a0a4205ee83dc8995c5f.

--- tg_bot/modules/sql/users_sql.py
+++ tg_bot/modules/sql/users_sql.py
@@ -138,6 +138,14 @@ def get_user_num_chats(user_id):
         SESSION.close()
 
 
+def get_user_com_chats(user_id):
+    try:
+        chat_members = SESSION.query(ChatMembers).filter(ChatMembers.user == int(user_id)).all()
+        return [i.chat for i in chat_members]
+    finally:
+        SESSION.close()
+
+
 def num_chats():
     try:
         return SESSION.query(Chats).count()
@@ -148,13 +156,6 @@ def num_chats():
 def num_users():
     try:
         return SESSION.query(Users).count()
-    finally:
-        SESSION.close()
-
-def get_user_com_chats(user_id):
-    try:
-        chat_members = SESSION.query(ChatMembers).filter(ChatMembers.user == int(user_id)).all()
-        return [i.chat for i in chat_members]
     finally:
         SESSION.close()
 
