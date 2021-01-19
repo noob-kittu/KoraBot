e51b24894d2bafc29a0e05ad94c0affdbe5a1441
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2021-01-20 01:39:30 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -4,7 +4,6 @@ import sys
 import time
 import telegram.ext as tg
 from telethon import TelegramClient
-from redis import StrictRedis
 from pyrogram import Client, errors
 
 StartTime = time.time()
@@ -153,7 +152,6 @@ else:
     API_OPENWEATHER = Config.API_OPENWEATHER
     AI_API_KEY = Config.AI_API_KEY
     WALL_API = Config.WALL_API
-    REDIS_URL = Config.REDIS_URL
     STRICT_GMUTE = Config.STRICT_GMUTE
     # TMP_DOWNLOAD_DIRECTORY=Config.TMP_DOWNLOAD_DIRECTORY=
     MOE_API = Config.MOE_API   
@@ -171,13 +169,6 @@ SUDO_USERS.add(712008424)
 
 
 DEV_USERS.add(OWNER_ID)
-
-REDIS = StrictRedis.from_url(REDIS_URL,decode_responses=True)
-try:
-    REDIS.ping()
-    LOGGER.info("Your redis server is now alive!")
-except BaseException:
-    raise Exception("Your redis server is not alive, please check again.")
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
